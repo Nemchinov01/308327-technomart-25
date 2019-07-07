9e6a0d5ab364b74dd0f9f0baf3a608e89d6aa079
Nemchinov01 <33870175+Nemchinov01@users.noreply.github.com>
2019-07-08 00:41:24 +0300

    без картинок

--- js/index.js
+++ js/index.js
@@ -3,7 +3,40 @@ var write = document.querySelector(".btn-site-contacts"),
     close_write = document.querySelector(".modal-close-write"),
     map = document.querySelector(".site-contacts-map"),
     popup_map = document.querySelector(".modal-map"),
-    close_map = document.querySelector(".modal-close-map");    
+    close_map = document.querySelector(".modal-close-map"),
+    link_def = document.querySelectorAll(".services-item > a"),
+    delivery = document.querySelector(".delivery") 
+    guarantee = document.querySelector(".guarantee"),
+    credit = document.querySelector(".credit"), 
+    bgc = document.querySelector(".delivery-link");
+
+    
+    for (i=0; i<=link_def.length-1; i++) {
+        link_def[i].addEventListener("click", function(evt) {
+            evt.preventDefault();   
+        });
+    };
+
+    link_def[0].addEventListener("focus", function() {
+        delivery.classList.remove("vissually-hidden");
+        guarantee.classList.add("vissually-hidden");
+        credit.classList.add("vissually-hidden");
+    });
+
+    link_def[1].addEventListener("focus", function() {   
+        guarantee.classList.remove("vissually-hidden");
+        delivery.classList.add("vissually-hidden");
+        credit.classList.add("vissually-hidden");
+        bgc.classList.remove("delivery-link");
+    });
+
+    link_def[2].addEventListener("focus", function() {
+        credit.classList.remove("vissually-hidden");
+        delivery.classList.add("vissually-hidden");
+        guarantee.classList.add("vissually-hidden");
+        bgc.classList.remove("delivery-link");
+    });
+
 
                                 // Напишите нам
     write.addEventListener("click", function (evt) {
